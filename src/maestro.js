70a2e99baaa733427b928082aadd15bb310af96e
Pierre-Rudolf Gerlach <pierre@joshfire.com>
2011-12-22 04:52:50 +0100

    make http call to remote server to get tag content

--- src/maestro.js
+++ src/maestro.js
@@ -1,13 +1,42 @@
-var express = require("express")
+var express = require("express"),
+	http = require("http")
 
 var app = express.createServer();
+
+var serverHost = "diynoisebox.herokuapp.com"
 
 app.get('/', function(req, res){
     res.send("Let's play some music !");
 });
 
 app.get('/tag/:idTag', function(req, res){
-    res.send("Seen tag " + req.params.idTag);
+	var options = {
+	  host: serverHost,
+	  port: 80,
+	  path: '/tag/' + req.params.idTag,
+	  method: 'GET'
+	};
+
+	var data = ''
+
+	var apiReq = http.request(options, function(apiRes) {
+	  console.log('STATUS: ' + apiRes.statusCode);
+	  apiRes.setEncoding('utf8');
+	  apiRes.on('data', function (chunk) {
+	    console.log('BODY: ' + chunk);
+	    data += chunk;
+	  });
+	  apiRes.on('end', function () {
+		var content = JSON.parse(data);
+		if (null != content) {
+			res.send("Tqg " + req.params.idTag + ' plays ' + JSON.stringify(content.content))
+		}
+		else {
+		    res.send("Seen tag " + req.params.idTag + ' but no can haz content');
+		}
+	  });
+	});
+	apiReq.end();
 });
 
 app.get('/cmd/playpause', function(req, res){
