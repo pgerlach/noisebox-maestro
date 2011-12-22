1c425ee0465a2480e0a51029fa65cd4cd92c747c
Pierre-Rudolf Gerlach <pierre@joshfire.com>
2011-12-22 04:09:08 +0100

    all urls ready

--- src/maestro.js
+++ src/maestro.js
@@ -6,4 +6,28 @@ app.get('/', function(req, res){
     res.send("Let's play some music !");
 });
 
+app.get('/tag/:idTag', function(req, res){
+    res.send("Seen tag " + req.params.idTag);
+});
+
+app.get('/cmd/playpause', function(req, res){
+    res.send("play/pause");
+});
+
+app.get('/cmd/next', function(req, res){
+    res.send("next");
+});
+
+app.get('/cmd/prev', function(req, res){
+    res.send("prev");
+});
+
+app.get('/cmd/vplus', function(req, res){
+    res.send("vplus");
+});
+
+app.get('/cmd/vminus', function(req, res){
+    res.send("vminus");
+});
+
 app.listen(3000);
